838da432b08240a0637efbf8f876045702fb73f6
JatinPundir-0087 <jatinpundir87@gmail.com>
2026-03-21 17:37:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio Loaded!");
+
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("In the next step, we will connect this to a Serverless API!");
+});
